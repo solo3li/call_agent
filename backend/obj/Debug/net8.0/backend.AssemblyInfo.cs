f3edc890a29c6afb99d6afe71fdcc0e8767854c7
solo3li <hamed3alii.3@gmail.com>
2026-07-12 09:52:30 +0000

    نن

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50be9b56b9f28f21247146a839272bc6eae2030")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3bc35a628b91c291ba4ebf9aa2b21ab1eda661")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
