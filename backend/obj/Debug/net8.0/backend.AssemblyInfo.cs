891ec30fe57b13f1c43b65ad31bf9f755353256d
solo3li <hamed3alii.3@gmail.com>
2026-07-23 08:12:16 +0000

    chore: update database connection credentials in .env and regenerate build artifacts

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+819c3c09e6477f0dccca8165e79b2b92d5e7d811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5ce2f3f439be824a637a1f74eee90d226abc82")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
